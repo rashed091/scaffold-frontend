58a361bcb273921ae5b75e1f99096202f3af9837
mrasheduzzaman <rashed091nsu@gmail.com>
2022-09-10 16:28:31 +0600

    Fix lint and format

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-it('renders welcome message', async () =>
-{
-	render(<App />)
-	expect(screen.getByText('Hello, World!')).toBeInTheDocument()
-})
+it('renders welcome message', async () => {
+  render(<App />);
+  expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+  console.log('adad');
+});
