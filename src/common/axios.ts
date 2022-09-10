4e6d72f99cb38306bbba064d915a3dd885b86da4
mrasheduzzaman <rashed091nsu@gmail.com>
2022-09-11 01:48:10 +0600

    Add common axios api

---
+++ src/common/axios.ts
@@ -0,0 +1,35 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+
+import { API_URL, JWT_SECRET } from './config';
+
+function authRequestInterceptor(config: AxiosRequestConfig)
+{
+	const token = JWT_SECRET;
+	if (token)
+	{
+		config.headers.authorization = `${token}`;
+	}
+	config.headers.Accept = 'application/json';
+	return config;
+}
+
+export const axios = Axios.create({
+	baseURL: API_URL,
+});
+
+axios.interceptors.request.use(authRequestInterceptor);
+axios.interceptors.response.use(
+	(response) =>
+	{
+		return response.data;
+	},
+	(error) =>
+	{
+		/* TODO:
+		- use notification system of mantine library to alert user on error
+		- const message = error.response?.data?.message || error.message;
+		*/
+
+		return Promise.reject(error);
+	}
+);
