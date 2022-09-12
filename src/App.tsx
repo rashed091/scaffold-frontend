f1d8356ce13b446813b99f074dd9f7946eb529b0
mrasheduzzaman <rashed091nsu@gmail.com>
2022-09-13 01:31:20 +0600

    Add basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import {
-  AppShell,
-  Navbar,
-  Header,
-  Footer,
-  Aside,
-  Text,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-} from '@mantine/core';
-import { useState } from 'react';
+import { AppShell, Text, useMantineTheme } from '@mantine/core';
+
+import { BottomBar } from './layout/BottomBar';
+import { SideBar } from './layout/Sidebar';
+import { TopBar } from './layout/TopBar';
+
 function App() {
   const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
   return (
     <AppShell
       styles={{
@@ -25,49 +18,11 @@ function App() {
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
-      navbar={
-        <Navbar
-          p="md"
-          hiddenBreakpoint="sm"
-          hidden={!opened}
-          width={{ sm: 200, lg: 300 }}
-        >
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      footer={
-        <Footer height={60} p="md">
-          Application footer
-        </Footer>
-      }
-      header={
-        <Header height={70} p="md">
-          <div
-            style={{ display: 'flex', alignItems: 'center', height: '100%' }}
-          >
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
+      navbar={<SideBar />}
+      footer={<BottomBar />}
+      header={<TopBar />}
     >
-      <Text>Resize app to see responsive navbar in action</Text>
+      <Text>Containers</Text>
     </AppShell>
   );
 }
