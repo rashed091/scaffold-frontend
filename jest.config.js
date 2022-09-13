1d9e8eb55c0847fe2adab930c5413ecf804c467b
mrasheduzzaman <rashed091nsu@gmail.com>
2022-09-14 02:58:25 +0600

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -27,9 +27,7 @@ module.exports = {
     },
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sss|styl)$': '<rootDir>/__mock__/style.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mock__/mock.js',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.ts$': 'ts-jest',
