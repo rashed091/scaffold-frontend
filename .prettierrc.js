539c9042beb70dab8a4deba826eb4c5edc5bd8cf
mrasheduzzaman <rashed091nsu@gmail.com>
2022-09-17 20:37:38 +0600

    Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,31 @@
 module.exports = {
   semi: true,
-  trailingComma: 'es5',
+  trailingComma: 'all',
   singleQuote: true,
-  printWidth: 80,
+  printWidth: 120,
   tabWidth: 2,
   endOfLine: 'auto',
   bracketSpacing: true,
+  overrides: [
+    {
+      files: ['*.ts'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.tsx'],
+      options: {
+        parser: 'typescript',
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+      },
+    },
+    {
+      files: ['*.json'],
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
 };
